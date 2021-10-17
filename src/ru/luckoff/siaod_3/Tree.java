9175f800084a9a0f13e9aa13c1749f0859ce78a2
neluckoff <neluckoff@gmail.com>
2021-10-17 22:31:40 +0300

    Initial commit

---
+++ src/ru/luckoff/siaod_3/Tree.java
@@ -0,0 +1,113 @@
+package ru.luckoff.siaod_3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите желаемый корень дерева: ");
+        int n = in.nextInt();
+
+        Node node = new Node(n);
+        node.left = new Node(4);
+        node.left.left = new Node(2);
+        node.right = new Node(5);
+
+        System.out.println("Меню:" +
+                "\n1 - Добавить рандомный элемент слева" +
+                "\n2 - Вывести бинарное дерево слева-направо" +
+                "\n3 - Вывести бинарное дерево справа-налево" +
+                "\n4 - Узнать высоту дерева" +
+                "\n5 - Найти среднее арифмитическое дерева" +
+                "\n6 - Узнать количество ребер дерева");
+        while (true) {
+            System.out.print("Номер: ");
+            int num = in.nextInt();
+            switch (num) {
+                case 1:
+                    node.add(node);
+                    break;
+                case 2:
+                    node.outputLeft(node);
+                    break;
+                case 3:
+                    node.outputRight(node);
+                case 4:
+                    System.out.println("Высота дерева: " + node.highTree(node));
+                    break;
+                case 5:
+                    System.out.println("Среднее арифметическое: " + (node.calcKey(node) / node.count(node)));
+                    break;
+                case 6:
+                    System.out.println("Количество ребер: " + node.count(node));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+    void outputLeft(Node node) { //in-order
+        if (node != null) {
+            outputLeft(node.left);
+            System.out.println(node.key);
+            outputLeft(node.right);
+        }
+    }
+
+    void outputRight(Node node) { //in-order
+        if (node != null) {
+            outputRight(node.right);
+            System.out.println(node.key);
+            outputRight(node.left);
+        }
+    }
+
+    void add(Node node) {
+        Random random = new Random();
+        int elem = random.nextInt(90);
+        node.left.left.left = new Node(elem);
+        System.out.println("Слева был добавлен новый элемент: " + elem);
+    }
+
+    int highTree(Node node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(highTree(node.left), highTree(node.right));
+        }
+    }
+
+    int count(Node node) {
+        if (node != null) {
+            return count(node.left) + count(node.right) + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    int calcKey(Node node) {
+        if (node != null) {
+            return calcKey(node.left) + calcKey(node.right) + node.key;
+        } else {
+            return 0;
+        }
+    }
+}
+
+
